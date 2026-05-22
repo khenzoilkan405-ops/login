64344be0bb85c0cb2d3d816dfe1e390ceb8c6d48
Khenzxo_ch <khenzoilkan405@email.com>
2026-05-22 15:53:15 +0700

    pls

---
+++ src/components/Input.jsx
@@ -0,0 +1,21 @@
+function Input({
+  type,
+  placeholder,
+  value,
+  onChange,
+  name,
+}) {
+  return (
+    <input
+      type={type}
+      name={name}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      className="w-full border border-gray-300 rounded-lg px-4 py-3 outline-none focus:ring-2 focus:ring-blue-500"
+    />
+  );
+}
+
+
+export default Input;
